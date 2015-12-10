215e85fc2e41e44eb5e99726cadca2f2de2983e2
Herz <achwazer@gmail.com>
2015-12-10 10:42:35 -0600

    parse all xml files in scielo_package fix #1

--- xml2markup.py
+++ xml2markup.py
@@ -55,55 +55,67 @@ def img_converter(src_path, destination_path):
 		print('Usage:')
 		print('python img_converter.py <images_folder> <jpg_folder>')
 
+def xml2markup(source):
+	basepath = os.path.dirname(os.path.dirname(source))
+	basename = os.path.splitext(os.path.basename(source))[0]
+	srcpath = os.path.join(basepath, 'src')
+	scielo_markup = os.path.join(basepath, 'scielo_markup')
+	output_file = os.path.join(scielo_markup, basename+'.html')
+
+	# check and create paths
+	if not os.path.exists(scielo_markup):
+		os.makedirs(scielo_markup)
+	if not os.path.exists(srcpath):
+		os.makedirs(srcpath)
+
+	dom = etree.parse(source)
+	xslt = etree.XSLT(etree.parse('xml2sgml.xsl'))
+	newdom = None
+	try:
+		newdom = xslt(dom)
+	except Exception, e:
+		print 'xml2sgml.xsl'
+		print e
+		print source
+		exit(1)
+
+
+	dom = etree.fromstring(re.sub(r'<img src="(.+?)(\.tif|\.tiff|\.eps)"/>', r'<img src="\1.jpg"/>', etree.tostring(newdom, encoding='utf-8')))
+	xslt = etree.XSLT(etree.parse('sgml2markup.xsl'))
+	try:
+		newdom = xslt(dom)
+	except Exception, e:
+		print 'sgml2markup.xsl'
+		print e
+		print source
+		exit(1)
+	# write sgml.xml file
+	# with open(os.path.join(scielo_markup, basename+'.sgml.xml'), 'w') as f:
+	# 	f.write(etree.tostring(dom))
+	# copy source files
+	files = glob.iglob(os.path.join(basepath, 'scielo_package', basename+"*.*"))
+	for file in files:
+		if os.path.isfile(file) and os.path.splitext(os.path.basename(file))[1] != '.xml' and not os.path.isfile(os.path.join(srcpath, os.path.basename(file))):
+			# print file
+			shutil.copy2(file, srcpath)
+	#convert img files
+	img_converter(srcpath, srcpath)
+
+	# write file to scielo_markup
+	with open(output_file, 'w') as f:
+		f.write((etree.tostring(newdom, encoding='utf-8', pretty_print=True, method="html")))
+
 parser = argparse.ArgumentParser()
-parser.add_argument('dom_file', action='store', help='XML File')
-
+parser.add_argument('source', action='store', help='XML file or scielo_package dir')
 arguments = parser.parse_args()
-basepath = os.path.dirname(os.path.dirname(arguments.dom_file))
-basename = os.path.splitext(os.path.basename(arguments.dom_file))[0]
-srcpath = os.path.join(basepath, 'src')
-scielo_markup = os.path.join(basepath, 'scielo_markup')
-output_file = os.path.join(scielo_markup, basename+'.html')
 
-# check and create paths
-if not os.path.exists(scielo_markup):
-	os.makedirs(scielo_markup)
-if not os.path.exists(srcpath):
-	os.makedirs(srcpath)
+if 'markup_xml/scielo_package' not in arguments.source:
+	print ('Need markup_xml/scielo_package path struct')
 
-dom = etree.parse(arguments.dom_file)
-xslt = etree.XSLT(etree.parse('xml2sgml.xsl'))
-newdom = None
-try:
-	newdom = xslt(dom)
-except Exception, e:
-	print arguments.dom_file
-	exit(1)
+if os.path.isdir(arguments.source):
+	files = glob.iglob(os.path.join(arguments.source, "*.xml"))
+	for file in files:
+		xml2markup(file)
+else: 
+	xml2markup(arguments.source)
 
-
-dom = etree.fromstring(re.sub(r'<img src="(.+?)(\.tif|\.tiff|\.eps)"/>', r'<img src="\1.jpg"/>', etree.tostring(newdom, encoding='utf-8')))
-xslt = etree.XSLT(etree.parse('sgml2markup.xsl'))
-try:
-	newdom = xslt(dom)
-except Exception, e:
-	print arguments.dom_file
-	exit(1)
-# write sgml.xml file
-# with open(os.path.join(scielo_markup, basename+'.sgml.xml'), 'w') as f:
-# 	f.write(etree.tostring(dom))
-# copy source files
-files = glob.iglob(os.path.join(os.path.dirname(arguments.dom_file), basename+"*.*"))
-for file in files:
-	if os.path.isfile(file) and os.path.splitext(os.path.basename(file))[1] != '.xml' and not os.path.isfile(os.path.join(srcpath, os.path.basename(file))):
-		# print file
-		shutil.copy2(file, srcpath)
-#convert img files
-img_converter(srcpath, srcpath)
-
-# write file to scielo_markup
-with open(output_file, 'w') as f:
-	f.write((etree.tostring(newdom, encoding='utf-8', pretty_print=True, method="html")))
-
-# print otags.sub(r'<br/>\1', htags.sub(r'<\1>',rtags.sub(r'[\1]', etree.tostring(newdom))))
-# print(etree.tostring(newdom, pretty_print=True))
-# print (newdom)
